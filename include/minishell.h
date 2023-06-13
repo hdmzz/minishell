9368fd46ffd74d3bd9d5b15154b10f41490dd808
hugo <hdamitzi@student.42lyon.fr>
2023-06-13 17:33:43 +0200

    pas encore de cmd executable simplement bientot on trouve dans les repertoires de la variabl PATH le bon binaire on recombine le chemin et envoie a exec

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: hdamitzi <hdamitzi@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/11 23:45:59 by hdamitzi          #+#    #+#             */
-/*   Updated: 2023/06/13 16:20:31 by hdamitzi         ###   ########.fr       */
+/*   Updated: 2023/06/13 17:16:57 by hdamitzi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,9 @@
 # include <readline/history.h>
 # include <sys/types.h>
 # include <sys/wait.h>
+# include <signal.h>
+# include <dirent.h>
+# include <stdbool.h>
 
 //srcs/parser/parser.c
 int		parser(char *cmd);
--- srcs/exec/exec.c
+++ srcs/exec/exec.c
@@ -6,11 +6,45 @@
 /*   By: hdamitzi <hdamitzi@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/13 15:43:43 by hdamitzi          #+#    #+#             */
-/*   Updated: 2023/06/13 16:21:49 by hdamitzi         ###   ########.fr       */
+/*   Updated: 2023/06/13 17:18:23 by hdamitzi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
+
+static bool	found_cmd_path(char *cmd, char *to_comp)
+{
+	
+}
+
+static char	*get_cmd_path(char **to_search)
+{
+	char			*env;
+	char			**split_env;
+	DIR				*dir;
+	struct dirent	*entry;
+	int				i;
+
+	env = getenv("PATH");//recover te variable $PATH
+	i = 0;
+	if (env == NULL)
+	{
+		perror("getenv");
+		exit(EXIT_FAILURE);//we are in a child process so the exit could be fetched in te waitpid call
+	}
+	split_env = ft_split(env, 58);//58 is int for : in ascii
+	//for each path we are gonna open the directory with opendir
+	while (split_env[i])
+	{
+		dir = opendir(split_env[i]);
+		if (dir != NULL)
+		{
+			while ((entry = readdir(dir)) != NULL)
+				printf("%s\n", entry->d_name);
+		}
+		i++;
+	}
+}
 
 void	exec(char **cmd)
 {
@@ -22,11 +56,15 @@ void	exec(char **cmd)
 		perror("Fork issue");
 	}
 	if (pid > 0)
+	{
 		waitpid(pid, NULL, 0);
+		kill(pid, SIGTERM);
+	}
 	else
 	{
-		if ((execve(cmd[0], cmd, NULL)) == -1)
-			perror("Exec");
+		get_cmd_path(cmd);
+		// if ((execve(cmd[0], cmd, NULL)) == -1)
+		// 	perror("Exec");
 		exit(EXIT_SUCCESS);
 	}
 }
