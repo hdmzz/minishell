3e8c2958b792f22f31ab8867e926c7b3b9249015
hugo <hdamitzi@student.42lyon.fr>
2023-06-19 14:33:04 +0200

    enum of the diffferent tokens type plus struct of tokens value + type we are gonna create a linked list, each noode of the list is one charactere we take out the spaces, then recombine the literals

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: hdamitzi <hdamitzi@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/11 23:45:59 by hdamitzi          #+#    #+#             */
-/*   Updated: 2023/06/15 13:24:47 by hdamitzi         ###   ########.fr       */
+/*   Updated: 2023/06/19 14:20:45 by hdamitzi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,27 @@
 # include <signal.h>
 # include <dirent.h>
 # include <stdbool.h>
+
+typedef enum e_tokens_type
+{
+	literal,
+	single_quote,
+	doule_quote,
+	simple_redir_left,
+	simple_redir_right,
+	double_redir_left,
+	double_redir_right,
+	pipe,
+	dollar,
+	space,
+}			t_tokens_type;
+
+typedef struct s_token
+{
+	void			*value;
+	t_tokens_type	type;
+}				t_token;
+
 
 typedef struct s_shell
 {
