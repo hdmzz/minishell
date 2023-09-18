045e364ede803b4e51608e49a68cd35a1dfa1801
hdmzz <hdamitzian@gmail.com>
2023-09-19 00:22:56 +0200

    tri export et leaks readline pdt le prompt

--- srcs/utils/utils.c
+++ srcs/utils/utils.c
@@ -6,7 +6,7 @@
 /*   By: hdamitzi <hdamitzi@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/15 13:38:17 by hdamitzi          #+#    #+#             */
-/*   Updated: 2023/09/16 13:12:02 by hdamitzi         ###   ########.fr       */
+/*   Updated: 2023/09/19 00:16:20 by hdamitzi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,6 +75,8 @@ static void	free_all(t_shell *g_shell)
 		g_shell->pids = ft_free_ptr(g_shell->pids);
 	if (g_shell->io != NULL)
 		g_shell->io = ft_free_ptr(g_shell->io);
+	if (g_shell->start_buff != NULL)
+		g_shell->start_buff = ft_free_ptr(g_shell->start_buff);
 }
 
 void	ft_free_shell(t_shell *g_shell, int last_exit)
